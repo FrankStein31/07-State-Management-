2ec9e387067897cc3914043e2c8278e788314fe7
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-04-17 16:12:34 +0700

    Praktikum 2

---
+++ src/app/zustand_coba/page.js
@@ -0,0 +1,10 @@
+import Counter from "@/components/Counter";
+
+export default function Coba() {
+    return (
+        <div>
+            <h1>Zustand Counter</h1>
+            <Counter />
+        </div>
+    );
+}
