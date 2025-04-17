b5595d968a26fb72952bbfc6234418ca9647a51b
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-04-17 16:26:55 +0700

    praktikum 5

---
+++ src/app/url_coba/page.js
@@ -0,0 +1,10 @@
+import ProductFilter from "@/components/ProductFilter";
+
+export default function Coba() {
+    return (
+        <div>
+            <h1>Product List</h1>
+            <ProductFilter />
+        </div>
+    );
+}
